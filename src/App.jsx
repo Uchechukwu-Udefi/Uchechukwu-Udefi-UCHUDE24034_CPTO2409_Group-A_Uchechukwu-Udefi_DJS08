ab127c93f0e6be3b9e8c23f89a93686b1658e28c
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-10 01:06:48 +0200

    Create home and about pages and import in app

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,17 @@
  */
 
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "../pages/Home.jsx"
+import About from "../pages/About.jsx"
 
 function App() {
 
-  return (
-    <h1>Start here</h1>
-  )
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={< Home />} />
+      <Route path="/about" elements={< About />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
