91d9ac8db053f235d82b463f8bbc0fe3e6100ccf
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-10 18:59:03 +0200

    Return components in App function

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,21 @@ import About from "../pages/About.jsx"
 
 function App() {
 
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={< Home />} />
-      <Route path="/about" elements={< About />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/about" element={< About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+  
 }
 
 export default App
