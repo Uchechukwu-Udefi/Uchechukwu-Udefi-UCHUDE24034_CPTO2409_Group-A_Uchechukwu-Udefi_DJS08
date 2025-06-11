82f8256db3bfdf8acec616781c433eb4a617e73e
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-11 10:23:48 +0200

    Create server and van file

---
+++ pages/Vans.jsx
@@ -0,0 +1,8 @@
+
+export default function Vans() {
+    return (
+        <div>
+            <h1>We got the travel vans.</h1>
+        </div>
+    );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home.jsx"
 import About from "../pages/About.jsx"
+import Vans from "../pages/Vans.jsx"
 
 function App() {
 
@@ -26,11 +27,13 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={< Home />} />
         <Route path="/about" element={< About />} />
+        <Route path="/vans" element={< Vans/>} />
       </Routes>
     </BrowserRouter>
   )
