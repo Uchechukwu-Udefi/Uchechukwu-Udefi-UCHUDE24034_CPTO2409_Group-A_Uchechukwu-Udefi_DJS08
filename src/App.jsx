2b820960fbc8909d6f156a5c9c395570ddd8a257
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-12 11:25:03 +0200

    Create and update van detail element

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home.jsx"
 import About from "../pages/About.jsx"
 import Vans from "../pages/Vans.jsx"
+import VanDetail from "../pages/VanDetail.jsx"
 
 function App() {
 
@@ -33,7 +34,8 @@ function App() {
       <Routes>
         <Route path="/" element={< Home />} />
         <Route path="/about" element={< About />} />
-        <Route path="/vans" element={< Vans/>} />
+        <Route path="/vans" element={< Vans />} />
+        <Route path="/vans/:id" element={< VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
