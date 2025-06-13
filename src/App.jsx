7c57ad3931d00296e27a2dd723aa7033c8e04e59
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-13 10:34:53 +0200

    Create header and layout file and nest their route

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,19 @@ import Home from "../pages/Home.jsx"
 import About from "../pages/About.jsx"
 import Vans from "../pages/Vans.jsx"
 import VanDetail from "../pages/VanDetail.jsx"
+import Layout from "../components/Layout.jsx";
 
 function App() {
 
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={< Home />} />
-        <Route path="/about" element={< About />} />
-        <Route path="/vans" element={< Vans />} />
-        <Route path="/vans/:id" element={< VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
