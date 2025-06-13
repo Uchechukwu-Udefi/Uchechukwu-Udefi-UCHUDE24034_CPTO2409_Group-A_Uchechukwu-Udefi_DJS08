ad619e1f5d6b5f03ddd8023eb0ac2bba9f135981
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-13 12:19:56 +0200

    Create and link host components

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,33 @@
  * linked in the slides.
  */
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "../pages/Home.jsx"
 import About from "../pages/About.jsx"
 import Vans from "../pages/Vans.jsx"
 import VanDetail from "../pages/VanDetail.jsx"
-import Layout from "../components/Layout.jsx";
+import Dashboard from "../pages/host/Dashboard.jsx"
+import Income from "../pages/host/Income.jsx"
+import Reviews from "../pages/host/Reviews.jsx"
+import Layout from "../components/Layout.jsx"
+import HostLayout from "../components/HostLayout.jsx"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
